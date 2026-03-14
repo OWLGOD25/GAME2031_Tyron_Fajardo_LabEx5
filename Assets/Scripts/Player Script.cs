3a8238869b76d1d4683aa18dc36869b44d7cb212
Tyron Fajardo <tyronjf44@gmail.com>
2026-03-14 13:29:11 -0400

    adding code and switching the input

--- Assets/Scripts/Player Script.cs
+++ Assets/Scripts/Player Script.cs
@@ -2,15 +2,25 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float speed = 5f;
+    public Rigidbody2D rb;
+
+    private float moveInput;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        moveInput = Input.GetAxis("Horizontal");
     }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(moveInput * speed, rb.linearVelocity.y);
+    }
+
 }
